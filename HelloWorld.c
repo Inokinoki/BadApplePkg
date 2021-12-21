fa67abea862706c211aeabec147cc78834b33963
Inoki <veyx.shaw@gmail.com>
2021-12-21 19:58:47 +0100

    Add basic Bad Apple bin file reading

--- HelloWorld.c
+++ HelloWorld.c
@@ -130,61 +130,110 @@ UefiMain (
     }
   }
 
-  //Setup tick loop
-  // BadAppleFrames
-  EFI_EVENT TickEvent;
-  UINTN eventId;
-  gBS->CreateEvent(EVT_TIMER, 0, NULL, NULL, &TickEvent);
-	gBS->SetTimer(TickEvent, TimerPeriodic, EFI_TIMER_PERIOD_MILLISECONDS(3000));
-  // EFI_GRAPHICS_OUTPUT_BLT_PIXEL *temp;
-  // EFI_GRAPHICS_OUTPUT_BLT_PIXEL *DrawBuffer;
-  EFI_GRAPHICS_OUTPUT_BLT_PIXEL *SpriteSheet = NULL;
+  // Read files
+  SHELL_FILE_HANDLE BABinFileHandle;
+  CHAR16  *FullFileName;
+  FullFileName = ShellFindFilePath(L"BA.bin");
+  if (FullFileName == NULL) {
+    Print(L"Failed to find BadApple bin compression file.\n");
+    goto Cleanup;
+  }
+  Status = ShellOpenFileByName(FullFileName, &BABinFileHandle, EFI_FILE_MODE_READ, 0);
+  if (EFI_ERROR(Status)) {
+    Print(L"Failed to load BadApple bin compression file.\n");
+    goto Cleanup;
+  }
+
+  EFI_STRING StringPtr = NULL;
+  UINTN Length = StrLen (L"BadAppleIFrames\\image-000.bmp") + 1;
+  StringPtr = AllocateZeroPool (Length * sizeof (CHAR16));
   UINTN SpriteSheetSize;
   UINTN SpriteSheetHeight;
   UINTN SpriteSheetWidth;
+  EFI_GRAPHICS_OUTPUT_BLT_PIXEL *intraFrames[52];
+  for (int i = 0; i < 52; i+=1) {
+    intraFrames[i] = NULL;
+    UnicodeSPrint (StringPtr, Length * sizeof (CHAR16), L"BadAppleIFrames\\image-%03d.bmp", i + 1);
+    // Print(L"Loading key frame %d.\n", i + 1);
+    Status = LoadBMP(StringPtr, &intraFrames[i], &SpriteSheetHeight, &SpriteSheetWidth, &SpriteSheetSize);
+    if (EFI_ERROR(Status)) {
+      Print(L"Failed to load key frame.\n");
+      goto Cleanup;
+    }
+    // Print(L"Key frame %d loaded.\n", i + 1);
+  }
+
+  // Setup tick loop
+  // BadAppleFrames
+  Print(L"Setup tick loop.\n");
+  EFI_EVENT TickEvent;
+  UINTN eventId;
+  gBS->CreateEvent(EVT_TIMER, 0, NULL, NULL, &TickEvent);
+	gBS->SetTimer(TickEvent, TimerPeriodic, EFI_TIMER_PERIOD_MILLISECONDS(33));
+
+  EFI_GRAPHICS_OUTPUT_BLT_PIXEL *DrawBuffer;
+  UINTN ReadLength = 4;
   UINTN BeginX = (screenWidth - VIDEO_WIDTH) / 2;
   UINTN BeginY = (screenHeight - VIDEO_HEIGHT) / 2;
-  EFI_STRING  StringPtr = NULL;
-  UINTN Length = StrLen (L"BadAppleFrames\\image-3288.bmp") + 1;
-  StringPtr = AllocateZeroPool (Length * sizeof (CHAR16));
+  DrawBuffer = NULL; // = AllocateCopyPool(VIDEO_WIDTH * BMP_TILE_LENGTH * VIDEO_HEIGHT * BMP_TILE_LENGTH, BackgroundBuffer);
+  int intraFrameIndex = 0;
+  unsigned char Buffer[4];
+  int NeedRefresh = 1;
   for (int i = 0; i < 6573; i+=1) {
-    //Copy Background to DrawBuffer
-    // DrawBuffer = AllocateCopyPool(LevelWidth * SpriteLength * LevelHeight * SpriteLength, BackgroundBuffer);
-
-    // Get frame
-    /*
-    UINTN
-    EFIAPI
-    UnicodeSPrint (
-      OUT CHAR16        *StartOfBuffer,
-      IN  UINTN         BufferSize,
-      IN  CONST CHAR16  *FormatString,
-      ...
-      );
-    */
-    UnicodeSPrint (
-      StringPtr,
-      Length * sizeof (CHAR16),
-      L"BadAppleFrames\\image-%04d.bmp",
-      i
-    );
-    // Print(StringPtr);
-    Status = LoadBMP(StringPtr, &SpriteSheet, &SpriteSheetHeight, &SpriteSheetWidth, &SpriteSheetSize);
-
-    //Wait for tick
-    gBS->WaitForEvent(1, &TickEvent, &eventId);
-    if (EFI_ERROR(Status)) {
-      Print(StringPtr);
-    } else {
-      // ExtractBuffer(DrawBuffer, LevelWidth * SpriteLength, LevelHeight * SpriteLength, 0, 0, &temp, 512, 512);
-      graphicsProtocol->Blt(graphicsProtocol, SpriteSheet, EfiBltBufferToVideo, 0, 0, BeginX, BeginY, VIDEO_WIDTH, VIDEO_HEIGHT, 0);
-      //Free screen and temporary
-      // FreePool(temp);
-      // FreePool(DrawBuffer);
+    NeedRefresh = 1;
+    UINTN DecodedInt32;
+    ReadLength = 4;
+    Status = ShellReadFile(BABinFileHandle, &ReadLength, (void *)Buffer);
+    if (ReadLength != 4) {
+      // FIXME: How can this happens
     }
+    // Read as little endian
+    DecodedInt32 = ((Buffer[3] << 24) | (Buffer[2] << 16) | (Buffer[1] << 8) | Buffer[0]);
+    Print(L"Read %d bytes: 0x%04X.\n", ReadLength, DecodedInt32);
+    if (EFI_ERROR(Status)) {
+      Print(L"Read file failed at %d.\n", i);
+      goto Cleanup;
+    }
+    if ((DecodedInt32 & 0x80000000) != 0) {
+      if ((DecodedInt32 & 0x7FFF7FFF) == 0x7FFF7FFF) {
+        // Keep frame and do not refresh
+        NeedRefresh = 0;
+        Print(L"Not need refresh at %d.\n", i);
+      } else {
+        Print(L"Patching at %d.\n", i);
+        goto Cleanup;
+        do {
+          // UINTN PatchX = ((DecodedInt32 & 0x7FFF0000) >> 16), PatchY = (DecodedInt32 & 0x7FFF);
+          // TODO: Begin patch until 0xFFFFFFFF
+
+          // Read next
+          ReadLength = 4;
+          Status = ShellReadFile(BABinFileHandle, &ReadLength, (void *)Buffer);
+          // Read as little endian
+          DecodedInt32 = ((Buffer[3] << 24) | (Buffer[2] << 16) | (Buffer[1] << 8) | Buffer[0]);
+          Print(L"Read %d bytes: 0x%04X.\n", ReadLength, DecodedInt32);
+        } while ((DecodedInt32 & 0x7FFF7FFF) != 0x7FFF7FFF);
+      }
+    } else {
+      // Read one intra-frame
+      Print(L"Print key frame %d at %d.\n", intraFrameIndex, i);
+      DrawBuffer = intraFrames[intraFrameIndex++];
+    }
+
+    // Wait for tick
+    gBS->WaitForEvent(1, &TickEvent, &eventId);
+    if (NeedRefresh)
+      graphicsProtocol->Blt(graphicsProtocol, DrawBuffer, EfiBltBufferToVideo, 0, 0, BeginX, BeginY, VIDEO_WIDTH, VIDEO_HEIGHT, 0);
   }
-// Cleanup:
-  FreePool (StringPtr);
+Cleanup:
+  FreePool(StringPtr);
+  if (DrawBuffer != NULL) {
+    FreePool(DrawBuffer);
+  }
+  ShellCloseFile(&BABinFileHandle);
+  if (FullFileName != NULL) {
+    FreePool(FullFileName);
+  }
   gBS->CloseEvent(TickEvent);
   gST->ConOut->EnableCursor(gST->ConOut, TRUE);
   if (!EFI_ERROR(Status)) {
